abd63bed4e573149a764760593f2f2124f494a7d
siva <ksivakumar0211@gmail.com>
2024-02-10 16:53:32 +0530

    Initial changes are done...

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ComingSoon from './Components/ComingSoon';
+
+function App() {
+  return (
+    <div className="App">
+      <ComingSoon />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ComingSoon.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { ReactComponent as IconFacebook } from '../Assets/Svg/icons8-facebook.svg';
+import { ReactComponent as IconYoutube } from '../Assets/Svg/icons8-youtube.svg';
+import { ReactComponent as IconLinkedin } from '../Assets/Svg/linkedin.svg';
+
+
+const ComingSoon = () => {
+  const handleEmailLinkClick = () => {
+    window.location.href = 'mailto:info@nandalalainfotech.com';
+  };
+    return (
+       <>
+       <div className="card">
+        <div className="header">
+          <div className="logo">
+           <img className='nanda_logo' src={"./images/nandalala_logo.svg"}/>
+          </div>
+          <div className="social">
+            <a href="https://www.facebook.com/profile.php?id=61555301096609609" title="Facebook" target="_blank" rel="noopener noreferrer">
+              <IconFacebook className="icon1" />
+            </a>
+            <a href="https://www.youtube.com/watch?v=l3bmI7NdPfE" title="Linkedin" target="_blank" rel="noopener noreferrer">
+              <IconYoutube className="icon1" />
+            </a>
+        
+            <a href="https://www.linkedin.com/in/invoicefree-nandalala-91ab122a7/" title="Linkedin" target="_blank" rel="noopener noreferrer">
+              <IconLinkedin className="icon" />
+            </a>
+          </div>
+        </div>
+        <div className="content">
+          <div className="title-holder">
+            <h1>Get ready for the change.</h1>
+            <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
+          </div>
+          <a href="mailto:info@nandalalainfotech.com" onClick={handleEmailLinkClick}>
+            <div className="cta">Send us an email</div>
+          </a>
+        </div>
+        <div className="footer">
+          <span>powered by <a className="underlined" href="https://nandalalainfotech.com/" target="_blank" rel="noopener noreferrer">nandalalainfotech</a>  <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer"></a>  <a className="underlined" href="https://github.com/arkn98/coming-soon" title="GitHub repo" target="_blank" rel="noopener noreferrer"></a></span>
+        </div>
+      </div>
+       </>
+    );
+};
+
+export default ComingSoon; 
